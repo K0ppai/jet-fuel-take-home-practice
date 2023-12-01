9b51f54e42aa86afb6d7aefe69f61ca6a2b18138
koppai <psthu.koppai@gmail.com>
2023-12-01 19:26:18 +0630

    Fetch campaigns

--- app/components/AppCard.tsx
+++ app/components/AppCard.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import AppCardHeader from './AppCardHeader';
 
-const AppCard = () => {
+const AppCard = ({ category }: { category: object }) => {
+  const { campaign_name, campaign_icon_url, pay_per_install } = category as {
+    campaign_name: string;
+    campaign_icon_url: string;
+    pay_per_install: string;
+  };
+
   return (
     <div>
-      
+      <AppCardHeader
+        name={campaign_name}
+        icon={campaign_icon_url}
+        payPerInstall={pay_per_install}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AppCard
+export default AppCard;
